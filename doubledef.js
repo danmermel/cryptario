9bb1f65218ed735b01563da5324b8af9a5b5aa30
Daniel Mermelstein <danmermel@gmail.com>
2019-04-08 12:33:15 +0100

    added createSearchablePairs - fixes issue #13

--- doubledef.js
+++ doubledef.js
@@ -2,6 +2,21 @@ const analyzeDoubleDef = async function (clue) {
   return []
 }
 
+const createSearchablePairs = function (words) {
+  const retval = []
+  for (var i = 0; i < words.length - 1; i++) {
+    const first = words.slice(0, i + 1)
+    const second = words.slice(i + 1)
+    const obj = {
+      one: first,
+      two: second
+    }
+    retval.push(obj)
+  }
+  return retval
+}
+
 module.exports = {
-  analyzeDoubleDef: analyzeDoubleDef
+  analyzeDoubleDef: analyzeDoubleDef,
+  createSearchablePairs: createSearchablePairs
 }
--- doubledef.test.js
+++ doubledef.test.js
@@ -1,5 +1,34 @@
 const doubledef = require('./doubledef.js')
 
+test('createSearchablePairs: splits four word string correctly', function () {
+  const res = doubledef.createSearchablePairs(['a', 'b', 'c', 'd'])
+  expect(res).toEqual([
+    { one: [ 'a' ], two: [ 'b', 'c', 'd' ] },
+    { one: [ 'a', 'b' ], two: [ 'c', 'd' ] },
+    { one: [ 'a', 'b', 'c' ], two: [ 'd' ] }
+  ])
+})
+
+test('createSearchablePairs: splits three word string correctly', function () {
+  const res = doubledef.createSearchablePairs(['a', 'b', 'c'])
+  expect(res).toEqual([
+    { one: [ 'a' ], two: [ 'b', 'c' ] },
+    { one: [ 'a', 'b' ], two: [ 'c' ] }
+  ])
+})
+
+test('createSearchablePairs: splits two word string correctly', function () {
+  const res = doubledef.createSearchablePairs(['a', 'b'])
+  expect(res).toEqual([
+    { one: [ 'a' ], two: [ 'b' ] }
+  ])
+})
+
+test('createSearchablePairs: splits one word string correctly', function () {
+  const res = doubledef.createSearchablePairs(['a'])
+  expect(res).toEqual([])
+})
+
 test('analyzeDoubleDef: Yearn for quite a while (4)', async function () {
   const res = await doubledef.analyzeDoubleDef('Yearn for quite a while (4)')
   expect(res).toEqual([{
