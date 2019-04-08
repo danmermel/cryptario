96b36624c4dbb617ff73e19fb154378f8e53c289
Daniel Mermelstein <danmermel@gmail.com>
2019-04-08 13:10:51 +0100

    it works!

--- doubledef.js
+++ doubledef.js
@@ -1,3 +1,6 @@
+const datamuse = require('./datamuse.js')
+const utilities = require('./utilities.js')
+
 const analyzeDoubleDef = async function (clue) {
   return []
 }
@@ -16,7 +19,28 @@ const createSearchablePairs = function (words) {
   return retval
 }
 
+const comparePair = async function (first, second, pattern) {
+  const firstSynonyms = await datamuse.synonym(first)
+  // Throw away any that are not as long as the solution pattern
+  const filteredFirstSynonyms = firstSynonyms.filter(function (word) { return utilities.checkWordPattern(word, pattern) })
+
+  const secondSynonyms = await datamuse.synonym(second)
+  const filteredSecondSynonyms = secondSynonyms.filter(function (word) { return utilities.checkWordPattern(word, pattern) })
+
+  // now see if there are common synonyms in both arrays
+
+  var matches = []
+  for (var i in filteredFirstSynonyms) {
+    var word = filteredFirstSynonyms[i]
+    if (filteredSecondSynonyms.includes(word)) {
+      matches.push(word)
+    }
+  }
+
+  return matches
+}
 module.exports = {
   analyzeDoubleDef: analyzeDoubleDef,
+  comparePair: comparePair,
   createSearchablePairs: createSearchablePairs
 }
