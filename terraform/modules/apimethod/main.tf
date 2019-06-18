8b7fce95f014ceabb4a44c7511299f93d5426455
Daniel Mermelstein <danmermel@gmail.com>
2019-06-18 16:36:11 +0100

    fixing api method creation dependency

--- terraform/modules/apimethod/main.tf
+++ terraform/modules/apimethod/main.tf
@@ -65,7 +65,7 @@ resource "aws_api_gateway_method_response" "cryptario_200" {
 
 # create integration response
 resource "aws_api_gateway_integration_response" "cryptario_integration_response" {
-  depends_on = ["aws_api_gateway_method.cryptario_options_method"]
+  depends_on = ["aws_api_gateway_integration.cryptario_integration"]
   rest_api_id = "${var.api_id}"
   resource_id = "${aws_api_gateway_resource.cryptario_resource.id}"
   http_method = "${aws_api_gateway_method.cryptario_options_method.http_method}"
