5c2c7e618458e1e6fba3fdf6afed5664f9ac12b2
Daniel Mermelstein <danmermel@gmail.com>
2019-09-01 09:57:42 +0100

    front end homophones

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,6 @@
 const solve = async (clue, clueType) => {
   var obj = { 'clue': clue }
-  var response = await fetch('https://r5atfhsr91.execute-api.eu-west-1.amazonaws.com/stage/' + clueType, {
+  var response = await fetch('https://prod.remebit.com/' + clueType, {
      method: 'post',
      body: JSON.stringify(obj)
   })
@@ -20,14 +20,17 @@ var app = new Vue({
     analyze: async function () {
       const self = this
       self.solutions = []
-      solve(this.clue, 'anagram').then(function(anagramSolutions) {
-        self.solutions = self.solutions.concat(anagramSolutions)
+      solve(this.clue, 'anagram').then(function(solutions) {
+        self.solutions = self.solutions.concat(solutions)
       })
-      solve(this.clue, 'hiddenwords').then(function(hiddenwordsSolutions) {
-        self.solutions = self.solutions.concat(hiddenwordsSolutions)
+      solve(this.clue, 'hiddenwords').then(function(solutions) {
+        self.solutions = self.solutions.concat(solutions)
       })
-      solve(this.clue, 'doubledef').then(function(doubledefSolutions) {
-        self.solutions = self.solutions.concat(doubledefSolutions)
+      solve(this.clue, 'doubledef').then(function(solutions) {
+        self.solutions = self.solutions.concat(solutions)
+      })
+      solve(this.clue, 'homophones').then(function(solutions) {
+        self.solutions = self.solutions.concat(solutions)
       })
     }
   }
