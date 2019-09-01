bee42677fdaa8285e2ee2142efa4382fc063c89c
Daniel Mermelstein <danmermel@gmail.com>
2019-09-01 09:45:29 +0100

    silent npm install and file removal after prepare

--- lambda/anagram/prepare.sh
+++ lambda/anagram/prepare.sh
@@ -8,11 +8,11 @@ cp ../../*.js ../../config.json .
 # run 'npm install' in the container
 # the files appear on our node_modules (because it's mounted)
 rm -rf node_modules
-docker run -v "$PWD":/var/task lambci/lambda:build-nodejs8.10 npm install
+docker run -v "$PWD":/var/task lambci/lambda:build-nodejs8.10 npm install --silent
 
 # build the zip
 zip -r lambda.zip package.json *.js config.json node_modules/
 
 # tidy up
-rm hiddenWordIndicators.js hiddenwords.js doubledef.js anagramIndicators.js datamuse.js db.js anagram.js utilities.js *.test.js config.json
+rm homophone.js homophoneIndicators.js hiddenWordIndicators.js hiddenwords.js doubledef.js anagramIndicators.js datamuse.js db.js anagram.js utilities.js *.test.js config.json
 
