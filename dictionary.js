e45c7b24c163d0f6c3351666db3b9ea044115810
danmermel <danmermel@gmail.com>
2020-03-01 09:01:47 +0000

    Issue76again (#88)
    * deleting dictionaries
    
    * deleting dictionaries
    
    * commiting the dictionaries and the generate scripts
    
    * deployment scripts
    
    * standard

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,15 @@
-const dictionary = require('./dictionary.json')
-
 const wordExists = (word) => {
+  word = word.trim()
+  word = word.toLowerCase()
+  word = word.replace(/[^a-z]/g, '')
+  const len = word.length
+  if (len === 0) {
+    return false
+  }
+  const dictionary = require('./dictionary' + len + '.json')
+  // the dictionary contains 'true' for valid entries but
+  // we need to ensure we return 'false' for missing entries
+  // to !! double-NOTs the answer to guarantee a boolean.
   return !!dictionary[word]
 }
 
--- scripts/generateDictionary.js
+++ scripts/generateDictionary.js
@@ -1,8 +1,26 @@
+const fs = require('fs')
+
 var main = function () {
   var LineByLineReader = require('line-by-line')
 
-  var lr = new LineByLineReader('combined.txt')
-  var dictionary = {}
+  var lr = new LineByLineReader('dictionary.txt')
+  var dictionary = {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {},
+    11: {},
+    12: {},
+    13: {},
+    14: {},
+    15: {}
+  }
 
   lr.on('error', function (err) {
     console.error(err)
@@ -14,18 +32,29 @@ var main = function () {
     if (line.length === 0) {
       return
     }
-    // no multi-word phrases
-    if (line.match(/ /)) {
-      return
-    }
     line = line.toLowerCase()
     // remove punctuation and numbers and shit
-    line = line.replace(/[^a-z]/g, '')
-    dictionary[line] = true
+    const word = line.replace(/[^a-z]/g, '')
+
+    // calculate how many letters are in the solution
+    // all solutions over 15 letters are bundled together
+    // so that the long tail on the bell curve are in one file
+    var len = Math.min(15, word.length)
+    var minidic = dictionary[len]
+
+    // does the jumble already exist as a key
+    if (!minidic[word]) {
+      minidic[word] = true // add it to the dictionary
+    }
   })
 
   lr.on('end', function () {
-    console.log(JSON.stringify(dictionary))
+    for (var i in dictionary) {
+      const filename = '../dictionary' + i + '.json'
+      console.log(filename)
+      fs.writeFileSync(filename, JSON.stringify(dictionary[i]))
+    }
+    // console.log(JSON.stringify(dictionary))
   })
 }
 
