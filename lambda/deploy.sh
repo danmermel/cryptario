e4c9fa5dcc6716226942982af7fb33f66aaa262e
Daniel Mermelstein <danmermel@gmail.com>
2020-03-05 13:50:26 +0000

    simplifying deployment steps and scripts

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -11,9 +11,15 @@ echo "configuring aws region"
 
 export AWS_DEFAULT_REGION="eu-west-1"
 
-# build the zip files
+# this sets the shell options so that ! is understood as NOT
+shopt -s extglob 
 
 echo "changing dir to build dir.."
+
+if [ -z $TRAVIS_BUILD_DIR ]
+then
+  TRAVIS_BUILD_DIR="."
+fi
 
 cd $TRAVIS_BUILD_DIR/lambda
 
@@ -22,49 +28,31 @@ echo $PWD
 # build the dictionaries
 echo "Building dictionaries"
 cd ../scripts
-npm install
-node generateAnagrams.js
-node generateDictionary.js
+#npm install
+#node generateAnagrams.js
+#node generateDictionary.js
 cd ../lambda
 echo "Done"
 
 # build the Lambda functions
-cd  anagram
-./prepare.sh
-cd ../hiddenwords
-./prepare.sh
-cd ../doubledef
-./prepare.sh
-cd ../homophones
-./prepare.sh
-cd ../reversals
-./prepare.sh
-cd ../containers
-./prepare.sh
-cd ../subtractions
-./prepare.sh
-cd ../charades
-./prepare.sh
-cd ..
+declare -a arr=("anagram" "hiddenwords" "doubledef" "homophones" "reversals" "containers" "charades" "subtractions")
+
+for i in "${arr[@]}"
+do
+  cd "$i"
+  echo "Doing ${i}"
+  pwd
+  cp ../../*.js ../../config.json ../../dictionary*.json ../../anagramSolutions*.json .
+  npm install
+  # build the zip
+  rm lambda.zip
+  zip -r lambda.zip *.js *.json node_modules/
+  # tidy up
+  # remove all files except those in the list
+  rm !(package.json|index.js|prepare.sh|node_modules|package-lock.json|lambda.zip)
+  # deploy to lambda
+  aws lambda update-function-code --function-name "cryptario-${i}-${1}" --zip-file fileb://lambda.zip
+  cd ..
+done
 
 
-# deploy to Lambda
-aws lambda update-function-code --function-name "cryptario-anagram-${1}" --zip-file fileb://anagram/lambda.zip
-aws lambda update-function-code --function-name "cryptario-doubledef-${1}" --zip-file fileb://doubledef/lambda.zip
-aws lambda update-function-code --function-name "cryptario-hiddenwords-${1}" --zip-file fileb://hiddenwords/lambda.zip
-aws lambda update-function-code --function-name "cryptario-homophones-${1}" --zip-file fileb://homophones/lambda.zip
-aws lambda update-function-code --function-name "cryptario-reversals-${1}" --zip-file fileb://reversals/lambda.zip
-aws lambda update-function-code --function-name "cryptario-containers-${1}" --zip-file fileb://containers/lambda.zip
-aws lambda update-function-code --function-name "cryptario-subtractions-${1}" --zip-file fileb://subtractions/lambda.zip
-aws lambda update-function-code --function-name "cryptario-charades-${1}" --zip-file fileb://charades/lambda.zip
-
-
-# tidy up zip files
-rm anagram/lambda.zip
-rm hiddenwords/lambda.zip
-rm doubledef/lambda.zip
-rm homophones/lambda.zip
-rm reversals/lambda.zip
-rm containers/lambda.zip
-rm subtractions/lambda.zip
-rm charades/lambda.zip
