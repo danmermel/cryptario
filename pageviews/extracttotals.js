6d49cebb4f14ecd140b6058d2f12d00106946e63
Daniel Mermelstein <danmermel@gmail.com>
2020-05-24 10:32:58 +0000

    standard fix

--- pageviews/extracttotals.js
+++ pageviews/extracttotals.js
@@ -1,11 +1,11 @@
 const TOTALS_FILE = './totals.json'
 var totals = require(TOTALS_FILE)
 
-function pad(num, size) {
-    var s = "000000000" + num;
-    return s.substr(s.length-size);
+function pad (num, size) {
+  var s = '000000000' + num
+  return s.substr(s.length - size)
 }
 
-for(var i in totals) {
-  console.log(pad(totals[i],8) + '\t' + i)
+for (var i in totals) {
+  console.log(pad(totals[i], 8) + '\t' + i)
 }
--- pageviews/totaliser.js
+++ pageviews/totaliser.js
@@ -7,21 +7,21 @@ if (!process.argv[2]) {
 const TOTALS_FILE = './totals.json'
 var totals = require(TOTALS_FILE)
 const fs = require('fs')
-var LineByLineReader = require('line-by-line'),
-    lr = new LineByLineReader(process.argv[2]);
+var LineByLineReader = require('line-by-line')
+var lr = new LineByLineReader(process.argv[2])
 
 lr.on('line', function (line) {
   const bits = line.split(' ')
   const page = bits[0]
   const count = parseInt(bits[1])
-  if(totals[page]) {
-    totals[page] += count 
+  if (totals[page]) {
+    totals[page] += count
   } else {
     totals[page] = count
-  } 
-});
+  }
+})
 
 lr.on('end', function () {
-  fs.writeFileSync(TOTALS_FILE, JSON.stringify(totals), { encoding: 'utf8'})
+  fs.writeFileSync(TOTALS_FILE, JSON.stringify(totals), { encoding: 'utf8' })
   process.exit(0)
-});
+})
